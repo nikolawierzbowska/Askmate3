811b84d96137c94274ea75a10f7d1aed823d194d
Ilona Drzymała <idrzym@gmail.com>
2023-06-19 17:27:10 +0200

    changed part of the code from csv to database, get_sorted_questions

--- util.py
+++ util.py
@@ -1,24 +1,21 @@
-import datetime
 import connection
+import datetime
 import os
-
-
-def convert_timestamp_to_date(timestamp):
-    date = datetime.datetime.fromtimestamp(timestamp)
-    return date.strftime("%Y-%m-%d %H:%M:%S")
-
-
-def generated_id(csv_file):
-    data_questions = connection.read_dict_from_file(csv_file)
-    existing_id = [int(line["id"]) for line in data_questions]
-    return max(int(identification) for identification in existing_id) + 1 if existing_id else 0
+import uuid
 
 
 def get_time():
-    current_date = datetime.datetime.now()
-    return int(datetime.datetime.timestamp(current_date))
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
+def save_image_dm(image_file):
+    unique_filename = str(uuid.uuid4()) + os.path.splitext(image_file.filename)[1]
+    image_path = 'static/uploads/' + unique_filename
+    image_file.save(image_path)
+    return image_path
+
+
+# TODO update funkcji pod baza danych
 def delete_image_file(file_path):
     if os.path.exists(file_path):
         os.remove(file_path)
