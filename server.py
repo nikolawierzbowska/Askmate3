47a394e656f68fb04683154a40de9bc7c61f1fe1
nikolawierzbowska <nikolawierzbowska21@gmail.com>
2023-06-21 20:45:30 +0200

    update askmate 1 to askmate 2 with database

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import flask
 from flask import Flask
 import data_manager
+import util
 
 app = Flask(__name__)
 
@@ -10,27 +11,24 @@ def main_page():
     return flask.redirect("/list")
 
 
-# @app.route('/list')
-# def list_questions():
-#     order_by = flask.request.args.get('order_by')
-#     order_direction = flask.request.args.get('order_direction')
-#     questions = data_manager.get_sorted_questions(order_by, order_direction)
-#     return flask.render_template("list.html", questions=questions)
-
-
 @app.route('/list')
-def print_questions():
-    questions = data_manager.list_questions_()
-    return flask.render_template('list.html', questions=questions)
+def list_questions():
+    order_by = flask.request.args.get('order_by')
+    order = flask.request.args.get('order_direction')
+    if order is not None and order_by is not None:
+        questions = data_manager.get_sorted_questions(order_by, order)
+        return flask.render_template("list.html", questions=questions)
+    else:
+        questions = data_manager.get_sorted_questions("submission_time", "DESC")
+        return flask.render_template("list.html", questions=questions)
 
 
 @app.route('/question/<int:question_id>')
 def print_question(question_id):
-    # data_manager.view_question_dm(question_id)
+    data_manager.view_question_dm(question_id)
     question = data_manager.get_question_data_by_id_dm(question_id)
-    # answers = data_manager.get_answers_by_question_id_dm(question_id)
-    return flask.render_template('question.html', question=question)
-    # return flask.render_template('question.html', question=question, answers=answers)
+    answers = data_manager.get_answers_by_question_id_dm(question_id)
+    return flask.render_template('question.html', question=question, answers=answers)
 
 
 @app.route('/add_question', methods=['GET', 'POST'])
@@ -41,25 +39,97 @@ def add_question():
         image_file = flask.request.files['image']
         image_path = None
         if 'image' in flask.request.files and image_file.filename != '':
-            image_path = data_manager.save_image_dm(image_file)
-        new_question_id =  data_manager.add_question_dm({'title': title, 'message': message, 'image': image_path})
-        return flask.redirect(f'/question/{data_manager.get_newest_question_dm()}')
+            image_path = util.save_image_dm(image_file)
+        submission_time = util.get_time()
+        new_question_id = data_manager.add_question_dm(submission_time, title, message, image_path)
+        return flask.redirect(f'/question/{new_question_id}')
     else:
         return flask.render_template('add_question.html')
 
-#
-# @app.route('/question/<question_id>/new_answer', methods=['GET', 'POST'])
-# def add_answer(question_id):
-#     if flask.request.method == 'POST':
-#         message = flask.request.form['message']
-#         image_file = flask.request.files['image']
-#         image_path = None
-#         if 'image' in flask.request.files and image_file.filename != '':
-#             image_path = data_manager.save_image_dm(image_file)
-#         data_manager.add_answer_dm(message, question_id, image_path)
-#         return flask.redirect(f'/question/{question_id}')
-#     elif flask.request.method == 'GET':
-#         return flask.render_template('add_answer.html', question_id=question_id)
+
+@app.route('/question/<int:question_id>/new_answer', methods=['GET', 'POST'])
+def add_answer(question_id):
+    if flask.request.method == 'POST':
+        message = flask.request.form['message']
+        image_file = flask.request.files['image']
+        image_path = None
+        if 'image' in flask.request.files and image_file.filename != '':
+            image_path = util.save_image_dm(image_file)
+        submission_time = util.get_time()
+        data_manager.add_answer_dm(submission_time, message, question_id, image_path)
+        return flask.redirect(f'/question/{question_id}')
+    elif flask.request.method == 'GET':
+        return flask.render_template('add_answer.html', question_id=question_id)
+
+
+@app.route('/question/<int:question_id>/delete')
+def delete_question(question_id):
+    data_manager.delete_question_dm(question_id)
+    return flask.redirect('/list')
+
+
+@app.route('/answer/<int:answer_id>/delete')
+def delete_answer(answer_id):
+    question_id = data_manager.delete_answer_by_id(answer_id)
+    return flask.redirect(f'/question/{question_id}')
+
+
+@app.route('/question/<int:question_id>/edit', methods=['GET', 'POST'])
+def edit_question(question_id):
+    delete_image = None
+    question = data_manager.get_question_data_by_id_dm(question_id)
+    if flask.request.method == 'GET':
+        return flask.render_template('edit_question.html', question=question)
+    elif flask.request.method == 'POST':
+        title = flask.request.form['title']
+        message = flask.request.form['message']
+        new_image_file = flask.request.files['image']
+        old_image_path = question["image"]
+        remove_image_checkbox = flask.request.form.get('remove_image')
+        if 'image' in flask.request.files and new_image_file.filename != '':
+            delete_image = True
+            data_manager.update_question_dm(title, message, old_image_path,new_image_file, question_id, delete_image)
+        elif remove_image_checkbox:
+            data_manager.update_question_dm(title, message, old_image_path, new_image_file,question_id,
+                                            remove_image_checkbox)
+        elif not remove_image_checkbox:
+            data_manager.update_question_dm(title, message, old_image_path, new_image_file, question_id,
+                                            delete_image)
+        return flask.redirect(f'/question/{question_id}')
+
+
+
+@app.route('/question/<int:question_id>/vote_up')
+def vote_up_questions(question_id):
+    if flask.request.args.get("source") == "question":
+        data_manager.vote_on_question_dm(question_id, "up")
+        return flask.redirect(f'/question/{question_id}')
+    else:
+        data_manager.vote_on_question_dm(question_id, "up")
+        return flask.redirect('/list')
+
+
+@app.route('/question/<question_id>/vote_down')
+def vote_down_questions(question_id):
+    if flask.request.args.get("source") == "question":
+        data_manager.vote_on_question_dm(question_id, "down")
+        return flask.redirect(f'/question/{question_id}')
+    else:
+        data_manager.vote_on_question_dm(question_id, "down")
+        return flask.redirect('/list')
+
+
+@app.route('/answer/<answer_id>/vote_up')
+def vote_up_answers(answer_id):
+    question_id = data_manager.vote_on_answer_dm(answer_id,"up")
+    return flask.redirect(f'/question/{question_id}')
+
+
+@app.route('/answer/<answer_id>/vote_down')
+def vote_down_answers(answer_id):
+    question_id = data_manager.vote_on_answer_dm(answer_id, "down")
+    return flask.redirect(f'/question/{question_id}')
+
 
 
 # @app.route('/answer/<answer_id>/edit', methods=['GET', 'POST'])
@@ -79,86 +149,6 @@ def add_question():
 #             image_path = answer[IMAGE - 1]
 #             data_manager.update_answer_dm(answer[ID], message, image_path, answer[ID + 3])
 #         return flask.redirect(f'/question/{answer[ID + 3]}')
-
-#
-# @app.route('/question/<question_id>/delete')
-# def delete_question(question_id):
-#     data_manager.delete_question_dm(question_id)
-#     data_manager.delete_answer_by_question_id(question_id)
-#     return flask.redirect('/list')
-#
-#
-# @app.route('/answer/<answer_id>/delete')
-# def delete_answer(answer_id):
-#     question_id = data_manager.get_question_id(answer_id)
-#     data_manager.delete_answer_by_id(answer_id)
-#     return flask.redirect(f'/question/{question_id}')
-#
-#
-# @app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
-# def edit_question(question_id):  # delete file -> util
-#     delete_image = None
-#     question = data_manager.get_question_data_by_id_dm(question_id)
-#     if flask.request.method == 'GET':
-#         return flask.render_template('edit_question.html', question=question)
-#     elif flask.request.method == 'POST':
-#         title = flask.request.form['title']
-#         message = flask.request.form['message']
-#         image_file = flask.request.files['image']
-#         remove_image = flask.request.form.get('remove_image')
-#         if 'image' in flask.request.files and image_file.filename != '':
-#             unique_filename = str(uuid.uuid4()) + os.path.splitext(image_file.filename)[1]
-#             image_path = 'static/uploads/' + unique_filename
-#             image_file.save(image_path)
-#             delete_image = True
-#             data_manager.update_question_dm(title, message, image_path, question_id, delete_image)
-#         elif image_file.filename == '' and remove_image:
-#             image_path = None
-#             data_manager.update_question_dm(title, message, image_path, question_id, delete_image)
-#         elif not remove_image:
-#             image_path = question['image']
-#             data_manager.update_question_dm(title, message, image_path, question_id, delete_image)
-#         return flask.redirect(f'/question/{question_id}')
-#
-#
-# @app.route('/question/<question_id>/delete_image')
-# def delete_question_image(question_id):  # delete file -> util
-#     data_manager.delete_image(question_id)
-#     return flask.redirect(f'/question/{question_id}')
-#
-#
-# @app.route('/question/<question_id>/vote_up')
-# def vote_up_questions(question_id):
-#     if flask.request.args.get("source") == "question":
-#         data_manager.vote_on_question_dm(question_id, "up")
-#         return flask.redirect(f'/question/{question_id}')
-#     else:
-#         data_manager.vote_on_question_dm(question_id, "up")
-#         return flask.redirect('/list')
-#
-#
-# @app.route('/question/<question_id>/vote_down')
-# def vote_down_questions(question_id):
-#     if flask.request.args.get("source") == "question":
-#         data_manager.vote_on_question_dm(question_id, "down")
-#         return flask.redirect(f'/question/{question_id}')
-#     else:
-#         data_manager.vote_on_question_dm(question_id, "down")
-#         return flask.redirect('/list')
-#
-#
-# @app.route('/answer/<answer_id>/vote_up')
-# def vote_up_answers(answer_id):
-#     question_id = data_manager.get_question_id(answer_id)
-#     data_manager.vote_on_answer_dm(answer_id, "up")
-#     return flask.redirect(f'/question/{question_id}')
-#
-#
-# @app.route('/answer/<answer_id>/vote_down')
-# def vote_down_answers(answer_id):
-#     question_id = data_manager.get_question_id(answer_id)
-#     data_manager.vote_on_answer_dm(answer_id, "up")
-#     return flask.redirect(f'/question/{question_id}')
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -1,25 +1,22 @@
 import datetime
-import connection
+import uuid
 import os
-
-
-def convert_timestamp_to_date(timestamp):
-    date = datetime.datetime.fromtimestamp(timestamp)
-    return date.strftime("%Y-%m-%d %H:%M:%S")
-
-
-def generated_id(cursor, table):
-    query = "SELECT MAX(id) FROM {}.{}};".format("public", table)
-    cursor.execute(query)
-    result = cursor.fetchone()
-    max_id = result[0] if result[0] is not None else 0
-    return max_id + 1
-
+from pathlib import Path
 
 def get_time():
     return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+def save_image_dm(image_file):
+    unique_filename = str(uuid.uuid4()) + os.path.splitext(image_file.filename)[1]
+    image_path = 'static/uploads/' + unique_filename
+    image_file.save(image_path)
+    return image_path
 
 def delete_image_file(file_path):
     if os.path.exists(file_path):
         os.remove(file_path)
+
+
+def delete_image_files(image_paths):
+    [Path(image_path).unlink() for image_path in image_paths if image_path and
+     Path(image_path).is_file() and Path(image_path).exists()]
