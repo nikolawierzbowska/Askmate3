426257c5ffdcc4ca71ab7b9df64f99755a6f61da
LukaszKusmierz <luckilucek303@gmail.com>
2023-06-21 20:47:25 +0200

    modified entire code from sprint_1 in order to connect it to PostgreSQL_DB

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import datetime
-import connection
 import os
+import uuid
+from pathlib import Path
 
 
 def convert_timestamp_to_date(timestamp):
@@ -8,16 +9,18 @@ def convert_timestamp_to_date(timestamp):
     return date.strftime("%Y-%m-%d %H:%M:%S")
 
 
-def generated_id(csv_file):
-    data_questions = connection.read_dict_from_file(csv_file)
-    existing_id = [int(line["id"]) for line in data_questions]
-    return max(int(identification) for identification in existing_id) + 1 if existing_id else 0
+def save_image_dm(image_file):
+    unique_filename = str(uuid.uuid4()) + os.path.splitext(image_file.filename)[1]
+    image_path = 'static/uploads/' + unique_filename
+    image_file.save(image_path)
+    return image_path
 
 
 def get_time():
     return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
-def delete_image_file(file_path):
-    if os.path.exists(file_path):
-        os.remove(file_path)
+def delete_image_files(image_paths):
+    [Path(image_path).unlink() for image_path in image_paths if image_path and
+     Path(image_path).is_file() and Path(image_path).exists()]
+
