ebd8f0ac9a8a7c71d2c85c0863693579ba89c995
nikolawierzbowska <nikolawierzbowska21@gmail.com>
2023-06-24 15:24:55 +0200

    Function edit comments has been created, The file edit_comments as beed updated and updated other file html

--- server.py
+++ server.py
@@ -132,9 +132,6 @@ def vote_down_answers(answer_id):
     return flask.redirect(f'/question/{question_id}')
 
 
-@app.route('/comment/<comment_id>/edit.', methods=['GET', 'POST'])
-def edit_comment_to_question(question_id):
-    pass
 
 
 @app.route('/question/<question_id>/new_comment', methods=['GET', 'POST'])
@@ -186,11 +183,26 @@ def search():
 @app.route('/comments/<comment_id>/delete')
 def delete_comments(comment_id):
     if flask.request.args.get("data") == "answer":
-        question_id = data_manager.get_question_id_to_comment(comment_id)
+        question_id = data_manager.get_question_id_to_comment_answer(comment_id)
         data_manager.delete_comment_dm(comment_id)
     else:
         question_id = data_manager.delete_comment_dm(comment_id)
     return flask.redirect(f'/question/{question_id}')
+
+
+
+@app.route('/comment/<comment_id>/edit',methods =['GET', 'POST'])
+def edit_comment(comment_id):
+    if flask.request.method == 'GET':
+        comment = data_manager.get_comment_by_id(comment_id)
+        question_id = data_manager.get_question_id_by_comment_question_or_answer(comment_id)
+        return flask.render_template('edit_comments.html',comment =comment, question_id=question_id)
+    elif flask.request.method =='POST':
+        message = flask.request.form['message']
+        data_manager.edit_comment_dm(comment_id,message)
+        question_id = data_manager.get_question_id_by_comment_question_or_answer(comment_id)
+        return flask.redirect(f'/question/{question_id}')
+
 
 
 
